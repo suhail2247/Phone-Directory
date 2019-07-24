0416656df5a008798343b35873d16ba9aed54a63
suhail2247 <suhail.chouhan786@gmail.com>
2019-07-25 01:48:09 +0530

    Structure of page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <div>Phone Directory</div>
+      <button>ADD</button>
+      <div>
+        <span>Name</span>
+        <span>Phone</span>
+      </div>
+    </div>
   );
 }
 
